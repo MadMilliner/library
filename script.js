45c9cab21d038d67ca07250daae6fc6c823ca476
MadMilliner <jasonapeterson@gmail.com>
2025-07-01 16:19:13 -0700

    I'm a little stuck with getting the form paramaters into the new books

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const myLibrary = []
 const library = document.getElementById('library');
+var addBookButton = document.getElementById('addBook');
+var newTitle = document.getElementById('newTitle').value;
+var newAuthor = document.getElementById('newAuthor').value;
+var newPages = document.getElementById('newPages').value;
+// var newRead = document.getElementById('newRead').value;
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = Number(pages);
-    this.read = read;
+    this.read = "No";
     this.id = crypto.randomUUID();
     
     if (!new.target) {
@@ -13,8 +18,8 @@ function Book(title, author, pages, read) {
     }
 };
 
-function addBookToLibrary(title, author, pages, read) {
-  var title = new Book(title, author, pages, read);
+function addBookToLibrary(title, author, pages) {
+  var title = new Book(title, author, pages);
   myLibrary.push(title);
 };
 
@@ -22,28 +27,45 @@ function showLibrary() {
     myLibrary.forEach(book => {
         var bookCard = document.createElement("div");
         bookCard.classList.add("book");
-        bookCard.innerHTML = `<p>Title: <span class="bookTitle">${book.title}</span></p><p>Author: ${book.author}</p><p>Pages: ${book.pages}</p><p>Have I read: ${book.read}</p>`;
+        bookCard.setAttribute("id", `${book.id}`)
+        bookCard.innerHTML = `<p>Title: <span class="bookTitle">${book.title}</span></p><p>Author: ${book.author}</p><p>Pages: ${book.pages}</p><p>Have I read: ${book.read}</p><p><button id="${book.title}" onclick="deleteBook()">Delete</button><button onclick="readBook()">Toggle Read</button></p>`;
         library.appendChild(bookCard);
         
     });
 }
 
-function addBookButton(title, author, pages, read) {
-    addBookToLibrary(title, author, pages, read);
-    myLibrary.sort();
+function deleteBook() {
+    myLibrary.splice(`this.id`,1);
+    // myLibrary.sort();
+    library.innerHTML = "";
     showLibrary();
 }
 
-function handleSubmit(event) {
-  event.preventDefault();
-  var newBook =    document.getElementById("newBook");
-
-  addBookButton(title, author, pages, read)
+function readBook() {
+    if (this.id === "Yes") {this.id = "No"}
+    else {this.id = "Yes"}
 }
 
-addBookToLibrary("The Diary of a Young Girl", "Ann Frank", 283, "Yes");
-addBookToLibrary("Lord of the Rings", "J. R. R. Tolkein", 1216, "Yes");
-addBookToLibrary("The Glass Castle", "Jeanette Walls", 288, "No");
+function addBookFunction(title, author, pages) {
+    addBookToLibrary(title, author, pages);
+    library.innerHTML = "";
+    myLibrary.sort();
+    showLibrary();
+};
+
+addBookButton.onclick = function(button) {
+  button.preventDefault();
+  title = newTitle;
+  author = newAuthor;
+  pages = newPages;
+//   read = newRead;
+
+  addBookFunction(title, author, pages)
+};
+
+addBookToLibrary("The Diary of a Young Girl", "Ann Frank", 283);
+addBookToLibrary("Lord of the Rings", "J. R. R. Tolkein", 1216);
+addBookToLibrary("The Glass Castle", "Jeanette Walls", 288);
 
 showLibrary();
 
