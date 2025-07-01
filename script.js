58b1f3b4b5843237165f988229a4baa07236e9a0
MadMilliner <jasonapeterson@gmail.com>
2025-07-01 12:26:46 -0700

    It's really coming along. Need to add the function to the form next

--- script.js
+++ script.js
@@ -16,25 +16,28 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
   var title = new Book(title, author, pages, read);
   myLibrary.push(title);
-//   sort(myLibrary);
-};
-
-Book.prototype.info = function() {
-    console.log('${this.title} by ${this.author}, ${this.pages} pages ${this.read}')
 };
 
 function showLibrary() {
     myLibrary.forEach(book => {
-        var book = document.createElement("div");
-        book.classList.add("book");
-        // book.setAttribute('id', this.title);
-        book.innerHTML = '<p>Title: ${this.title}</p><p>Author: ${this.author}</p><p>Pages: ${this.pages}</p><p>Have I read: ${this.read}</p>';
-        library.appendChild(book);
+        var bookCard = document.createElement("div");
+        bookCard.classList.add("book");
+        bookCard.innerHTML = `<p>Title: ${book.title}</p><p>Author: ${book.author}</p><p>Pages: ${book.pages}</p><p>Have I read: ${book.read}</p>`;
+        library.appendChild(bookCard);
         
     });
 }
 
-addBookToLibrary("Bible", "various", 1000, "Have Read");
-addBookToLibrary("LordoftheRings", "J. R. R. Tolkein", 750, "Have Read");
+addBookToLibrary("The Diary of a Young Girl", "Ann Frank", 283, "Yes");
+addBookToLibrary("Lord of the Rings", "J. R. R. Tolkein", 1216, "Yes");
+addBookToLibrary("The Glass Castle", "Jeanette Walls", 288, "No");
 
 showLibrary();
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+} 
