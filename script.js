6cc82af1d5a7bec779c424430e0d98466fbc5221
MadMilliner <jasonapeterson@gmail.com>
2025-07-02 10:49:29 -0700

    Fin

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const myLibrary = []
 const library = document.getElementById('library');
-var addBookButton = document.getElementById('addBook');
-var newTitle = document.getElementById('newTitle').value;
-var newAuthor = document.getElementById('newAuthor').value;
-var newPages = document.getElementById('newPages').value;
+const addBookButton = document.getElementById('addBook');
+const newTitle = document.getElementById('newTitle').value;
+const newAuthor = document.getElementById('newAuthor').value;
+const newPages = document.getElementById('newPages').value;
+
 // var newRead = document.getElementById('newRead').value;
 
-function Book(title, author, pages,) {
+function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = Number(pages);
-    this.read = "No";
+    this.read = false;
     this.id = crypto.randomUUID();
     
     if (!new.target) {
@@ -18,41 +19,48 @@ function Book(title, author, pages,) {
     }
 };
 
-Book.prototype.readToggle = function() {
-  if (this.read === "Yes") {this.read = "No"}
-    else {this.read = "Yes"}
-}
-
-function readBook() {
-    // button.preventDefault();
-    this.Book.readToggle();
-    library.innerHTML = "";
-    showLibrary();
-
-}
-
-function addBookToLibrary(title, author, pages) {
-  var title = new Book(title, author, pages);
-  myLibrary.push(title);
-};
 
 function showLibrary() {
     myLibrary.forEach(book => {
         var bookCard = document.createElement("div");
         bookCard.classList.add("book");
         bookCard.setAttribute("id", `${book.id}`)
-        bookCard.innerHTML = `<p>Title: <span class="bookTitle">${book.title}</span></p><p>Author: ${book.author}</p><p>Pages: ${book.pages}</p><p>Have I read: ${book.read}</p><p><button id="${book.title}" onclick="deleteBook()">Delete</button><button onclick="readBook()">Read</button></p>`;
+        bookCard.innerHTML = `<p>Title: <span class="bookTitle">${book.title}</span></p><p>Author: ${book.author}</p><p>Pages: ${book.pages}</p><p class="have-read">Have I read: ${book.getReadStatus()}</p><p><button id="${book.id}" onclick="readBook('${book.id}')">Read</button><button id="${book.title}" onclick="deleteBook()">Delete</button></p>`;
         library.appendChild(bookCard);
         
     });
-}
+  }
 
-function deleteBook() {
-    myLibrary.splice(`this.id`,1);
-    // myLibrary.sort();
+function deleteBook(id) {
+    const bookIndex = myLibrary.findIndex(book => book.id === id); // FIXED: Proper way to find and remove book
+    if (bookIndex > -1) {
+        myLibrary.splice(bookIndex, 1);
+    }
     library.innerHTML = "";
     showLibrary();
-}
+};
+
+Book.prototype.readToggle = function() {
+    this.read = !this.read
+};
+
+Book.prototype.getReadStatus = function() {
+    return this.read ? "Yes" : "No";
+};
+
+function readBook(id) {
+    const thisBook = myLibrary.find((book) => book.id === String(id));
+    if (thisBook) { // FIXED: Added null check
+        thisBook.readToggle();
+        library.innerHTML = "";
+        showLibrary();
+    }
+};
+
+function addBookToLibrary(title, author, pages) {
+  var newBook = new Book(title, author, pages);
+  myLibrary.push(newBook);
+};
 
 function addBookFunction(title, author, pages) {
     addBookToLibrary(title, author, pages);
@@ -66,21 +74,21 @@ addBookButton.onclick = function(button) {
   let title = newTitle;
   let author = newAuthor;
   let pages = newPages;
-//   let read = newRead;
 
   addBookFunction(title, author, pages);
 };
 
-// addBookToLibrary("The Diary of a Young Girl", "Ann Frank", 283, "Yes");
-// addBookToLibrary("Lord of the Rings", "J. R. R. Tolkein", 1216, "Yes");
-// addBookToLibrary("The Glass Castle", "Jeanette Walls", 288, "No");
+addBookToLibrary("The Diary of a Young Girl", "Ann Frank", 283, "Yes");
+addBookToLibrary("Lord of the Rings", "J. R. R. Tolkein", 1216, "Yes");
+addBookToLibrary("The Glass Castle", "Jeanette Walls", 288, "No");
+addBookToLibrary("Alice's in Wonderland", "Lewis Carrol", 320);
 
 showLibrary();
 
 function openForm() {
   document.getElementById("myForm").style.display = "block";
-}
+};
 
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
-} 
+};
